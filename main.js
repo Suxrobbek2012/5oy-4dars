c6ff13df7926d032fb0b3c4175fd083bd4154330
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-02-11 11:23:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let ism = prompt("ismingizni kriting");
+let harif = prompt(" harif kriting");
+console.log(ism);
+
+alert(`${ism} ismda ${harif.includes("")} harf mavjud`);
+
+
+// const userName = "suxrobbek";
+// const result = userName.length Harf sanaydi
+// const result = userName.indexOf("b",2)
+// const result = userName[5]
+// const result = userName.toLowerCase();
+// const result = userName.toUpperCase();
+// console.log(result);
+
+// let bot = prompt("ismingizni kriting");
+// alert(`${bot} ismda ${bot.length} ta harf bor`);
+
